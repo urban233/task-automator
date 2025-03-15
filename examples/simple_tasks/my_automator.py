d0dc2e75e02b2c01bb7b4a53796a0dd5c5c9016f
Martin Urban <martin.urban@studmail.w-hs.de>
2025-03-15 17:33:25 +0100

    Update examples

--- examples/simple_tasks/my_automator.py
+++ examples/simple_tasks/my_automator.py
@@ -15,27 +15,7 @@
 #-*
 #Z* -------------------------------------------------------------------
 """
-
-
-def setup_dev_env():
-    print("Setting up dev environment...")
-
-
-def build_win_package():
-    print("Building Windows package...")
-
-
-def clean_build_setup_exe():
-    print("Cleaning build setup exe...")
-
-
-def build_setup_exe():
-    print("Building setup exe...")
-
-
-def build_update_src_only_exe():
-    print("Building update source only exe...")
-
+import tasks
 
 AUTOMATION_TREE: dict = {
     "build": {
@@ -43,15 +23,15 @@ AUTOMATION_TREE: dict = {
         "subcommands": {
             "win-package": {
                 "help": "Build Windows ZIP package",
-                "func": build_win_package
+                "func": tasks.build_win_package
             },
             "setup-exe": {
                 "help": "Build Inno Setup EXE",
-                "func": build_setup_exe
+                "func": tasks.build_setup_exe
             },
             "update-src-exe": {
                 "help": "Build update source Inno Setup EXE",
-                "func": build_update_src_only_exe
+                "func": tasks.build_update_src_only_exe
             },
             # ... other build subcommands ...
         }
